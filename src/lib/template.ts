24964334fb2b0a4f82d54e9c6211c6e700bef47a
7sDream <i@7sdre.am>
2021-04-22 00:21:34 +0800

    style(icon,sendpanel): add icon, fix sendpanel css

--- src/lib/template.ts
+++ src/lib/template.ts
@@ -16,7 +16,7 @@ const getTemplate = async (name: OPTIONS): Promise<string> => {
 const dateFormatter = (date: Date) => {
     const year = date.getFullYear().toString().padStart(4, "0");
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    const day = date.getDay().toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
     const hour = date.getHours().toString().padStart(2, "0");
     const minute = date.getMinutes().toString().padStart(2, "0");
     const second = date.getSeconds().toString().padStart(2, "0");
